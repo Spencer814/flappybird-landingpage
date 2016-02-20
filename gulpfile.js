76e4cdd908c30104b74088fd94c4f17552150694
Andre Newman <Spencer814@users.noreply.github.com>
2016-02-20 13:54:29 -0500

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
+var HTMLmin = require('gulp-htmlmin');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+// JavaScript linting task
+gulp.task('jshint', function() {
+	return gulp.src('js/*.js')
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'));
+});
